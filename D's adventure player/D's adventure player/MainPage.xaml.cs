e91f6d7cdf21f6f6c1731a13b4c01d2f13ef6515
mkaraki <mkaraki_apps_publisher@outlook.jp>
2018-07-07 01:59:28 +0900

    Can play from Picked Source

--- D's adventure player/D's adventure player/MainPage.xaml.cs
+++ D's adventure player/D's adventure player/MainPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Playback;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -20,11 +23,31 @@ namespace D_s_adventure_player
     /// <summary>
     /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
     /// </summary>
+
+
     public sealed partial class MainPage : Page
     {
+        public MediaPlayer player = new MediaPlayer();
+
+        public Windows.Storage.StorageFile playfile;
+
         public MainPage()
         {
             this.InitializeComponent();
+            //player.Source = MediaSource.CreateFromUri(new Uri(@"ms-appdata:///debug.wav"));
+        }
+
+        private void play_Click(object sender, RoutedEventArgs e)
+        {
+            player.Source = MediaSource.CreateFromStorageFile(playfile);
+            player.Play();
+        }
+
+        private async void open_Click(object sender, RoutedEventArgs e)
+        {
+            var filePicker = new Windows.Storage.Pickers.FileOpenPicker();
+            filePicker.FileTypeFilter.Add(".wav");
+            playfile = await filePicker.PickSingleFileAsync();
         }
     }
 }
